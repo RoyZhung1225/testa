5e383da63fc223ad73846b02d90f45575464a03b
RoyZhung1225 <royzhung561@gmail.com>
2025-11-20 19:19:43 +0800

    第一版本

---
+++ src/main/java/org/example/GUI.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javax.swing.*;
+
+public class GUI {
+    private JTextArea textArea1;
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JButton button1;
+    private JButton button2;
+    private JTextArea textArea2;
+    private JButton button3;
+    private JComboBox comboBox1;
+    private JTextArea textArea3;
+    private JButton button4;
+}
